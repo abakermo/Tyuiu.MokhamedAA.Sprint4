b6efb4b95319c5bbdc3e3a77370e8e770ad475ff
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-04 21:52:34 +0300

    Task5

---
+++ Tyuiu.MokhamedAA.Sprint4.Task5.V10.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MokhamedAA.Sprint4.Task5.V10.Lib
+{
+    public class DataService : ISprint4Task5V10
+    {
+        public int Calculate(int[,] m)
+
+        {
+            int rows, columns, s;
+
+            rows = m.GetUpperBound(0) + 1;
+            columns = m.Length / rows;
+            s = 0;
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    if (m[i, j] > 0)
+                        s += m[i, j];
+            return s;
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.MokhamedAA.Sprint4.Task5.V10/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.MokhamedAA.Sprint4.Task5.V10.Lib;
+namespace Tyuiu.MokhamedAA.Sprint4.Task5.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Random rand = new Random();
+
+            
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+
+            int rows, columns;
+
+            Console.WriteLine("Введите количество строк в массиве: ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] m = new int[rows, columns];
+
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    m[i, j] = rand.Next(-4, 7);
+
+            Console.WriteLine("\nМассив");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{m[i, j]}\t");
+                }
+
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine(ds.Calculate(m));
+
+        }
+    }
+}
