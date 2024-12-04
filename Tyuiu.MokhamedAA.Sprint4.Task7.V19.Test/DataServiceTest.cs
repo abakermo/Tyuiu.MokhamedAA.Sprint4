93413ddbd8a8633cbb79c4b2aab1df72ff08a7c2
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-04 22:05:13 +0300

    Task7

---
+++ Tyuiu.MokhamedAA.Sprint4.Task7.V19.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MokhamedAA.Sprint4.Task7.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MokhamedAA.Sprint4.Task7.V19/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.MokhamedAA.Sprint4.Task7.V19.Lib;
+namespace Tyuiu.MokhamedAA.Sprint4.Task7.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[,] matrix = new int[4, 2];
+            string str = "90817264";
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int index = 0;
+            Console.WriteLine("\nМассив");
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 2; j++, index++)
+                {
+                    Console.Write($"{str[index]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Количество нечётных элементов = " + ds.Calculate(4, 2, str));
+            Console.ReadKey();
+        }
+    }
+}
